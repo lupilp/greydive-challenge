52632c0ac3975bf987e4141d02c5b2a3f523789c
lupilp <lupilp97@gmail.com>
2022-11-25 11:13:13 -0300

    todo ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import db from "../db/db.json";
+import style from "../style/Home.module.css";
 
 export default function Home() {
   return <div>{db[0].cliente}</div>;
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import db from "../db/db.json";
+import style from "../style/Test.module.css";
 
 export default function Test() {
   return <div>Test</div>;
