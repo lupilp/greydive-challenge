e632473fbf3ec1a261227fdb26385d7e2a244fb4
lupilp <lupilp97@gmail.com>
2022-11-26 12:15:02 -0300

    finish

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import style from "../style/Card.module.css";
+import img from "../images/web.png";
+import arrow from "../images/arrow.png";
 
 export default function Card({ cliente, escenario, idVideo }) {
   return (
     <div className={style.card}>
-      {cliente}
+      <img src={img} alt="img" className={style.imagen} />
+      {/* <div className={style.cliente}>{cliente}</div> */}
       <div className={style.escenario}>{escenario}</div>
       <Link to={`/test/${idVideo}`}>
-        <div className={style.buttonIr}>Ir al test</div>
+        <div className={style.buttonIr}>
+          Ir al test
+          <img src={arrow} alt="arrow" className={style.arrow} />
+        </div>
       </Link>
     </div>
   );
